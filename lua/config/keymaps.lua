9cf0248a294607c00a18dd4e6320b770bbf027b4
snillepille <155476536+snillepille@users.noreply.github.com>
2025-10-14 01:59:52 +0100

    remove unnecessary toggleterm keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,13 +3,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- aliases
-vim.api.nvim_create_user_command("Lz", function() vim.cmd("Lazy") end, {desc = "Opens Lazy GUI"}) -- Lazy GUI
-
--- window keymaps
-vim.keymap.set({ 't', 'n', 'v' }, "§h", "<cmd>wincmd h<CR>", { desc = "Moves cursor to window left of current" })
-vim.keymap.set({ 't', 'n', 'v' }, "§j", "<cmd>wincmd j<CR>", { desc = "Moves cursor to window below current" })
-vim.keymap.set({ 't', 'n', 'v' }, "§k", "<cmd>wincmd k<CR>", { desc = "Moves cursor to window above current" })
-vim.keymap.set({ 't', 'n', 'v' }, "§l", "<cmd>wincmd l<CR>", { desc = "Moves cursor to window right of current" })
+vim.api.nvim_create_user_command("Lz", function() vim.cmd("Lazy") end, { desc = "Opens Lazy GUI" }) -- Lazy GUI
 
 -- tmux keymaps
 vim.keymap.set({ 'n' }, "<C>h", "<cmd> TmuxNavigateLeft<CR>", { desc = "tmux window left" })
