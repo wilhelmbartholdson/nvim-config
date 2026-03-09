882ea7625a94533673329793661637e8adeaf837
snillepille <155476536+snillepille@users.noreply.github.com>
2026-03-09 16:33:53 +0100

    add folding defaults + format comments

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,10 +6,10 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 -- Set general indentation rules
-vim.opt.tabstop = 4         -- A tab is 4 spaces
-vim.opt.shiftwidth = 4      -- Indentation width
-vim.opt.expandtab = true    -- Use spaces instead of tabs
-vim.opt.softtabstop = 4     -- Insert 4 spaces for a Tab
+vim.opt.tabstop = 4      -- A tab is 4 spaces
+vim.opt.shiftwidth = 4   -- Indentation width
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.softtabstop = 4  -- Insert 4 spaces for a Tab
 
 -- Set relative line numbers
 vim.opt.relativenumber = true
@@ -25,8 +25,12 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+-- folding defaults
+vim.opt.foldlevel = 99
+vim.opt.foldenable = false
+
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.wrap = true -- setting nowrap
+vim.opt.wrap = true       -- setting wrap
 vim.opt.sidescrolloff = 8 -- how many columns the cursor keeps from the side of the screen when sidescrolling
