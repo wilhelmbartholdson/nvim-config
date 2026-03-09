ccfca575d94d8d922817636fbc0ffcdf08feed73
snillepille <155476536+snillepille@users.noreply.github.com>
2026-03-09 16:30:53 +0100

    update treesitter + yank yanks to clipboard reg

--- init.lua
+++ init.lua
@@ -7,5 +7,30 @@ require("config.options")
 -- initialize lazy.nvim
 require("config.lazy")
 
+-- clipboard is in p (not "+ i.e +-registry)
+vim.opt.clipboard = "unnamedplus"
+
 -- initialize lsp
 require("config.lsp")
+
+-- nvim-treesitter
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {
+    'python',
+    'javascript',
+    'jsx',
+    'typescript',
+    'tsx',
+    'lua',
+    'html',
+    'css',
+    'markdown'
+  },
+  callback = function()
+    vim.treesitter.start()
+    vim.wo[0][0].foldmethod = 'expr'
+    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    vim.opt.foldenable = false -- disable folding on startup
+  end,
+})
