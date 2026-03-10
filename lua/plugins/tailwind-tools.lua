7c0933a9734613f8ec632d291b65dadb51af4684
snillepille <155476536+snillepille@users.noreply.github.com>
2026-03-10 14:12:47 +0100

    rm deprecated nvim-lspconfig

--- lua/plugins/tailwind-tools.lua
+++ lua/plugins/tailwind-tools.lua
@@ -6,7 +6,7 @@ return {
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-telescope/telescope.nvim", -- optional
-    "neovim/nvim-lspconfig",         -- optional
+    -- "neovim/nvim-lspconfig",         -- optional
   },
-  opts = {}                          -- your configuration
+  opts = {} -- your configuration
 }
