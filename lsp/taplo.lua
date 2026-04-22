10203ace96fc9302f33531233f9e9aade40ff015
snillepille <155476536+snillepille@users.noreply.github.com>
2026-04-22 17:20:08 +0200

    add sesh schema

--- lsp/taplo.lua
+++ lsp/taplo.lua
@@ -2,5 +2,13 @@ return {
   cmd = { "taplo", "lsp", "stdio" },
   filetypes = { "toml" },
   root_markers = { ".taplo.toml", "taplo.toml", ".git" },
-  settings = {},
+  settings = {
+    taplo = {
+      schema = {
+        associations = {
+          [".*sesh\\.toml$"] = "https://github.com/joshmedeski/sesh/raw/main/sesh.schema.json",
+        }
+      }
+    }
+  },
 }
