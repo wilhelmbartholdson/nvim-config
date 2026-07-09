7eddb44e941c4f1ca48177bc5731356005698b5c
snillepille <155476536+snillepille@users.noreply.github.com>
2026-07-09 18:47:27 +0200

    add sql lsp

---
+++ lsp/sqlls.lua
@@ -0,0 +1,14 @@
+return {
+  cmd          = { "sql-language-server", "up", "--method", "stdio" },
+  filetypes    = { "sql", "mysql" },
+  root_markers = {
+    'pyproject.toml',
+    'setup.py',
+    'setup.cfg',
+    'requirements.txt',
+    'Pipfile',
+    'pyrightconfig.json',
+    '.git', ".sqllsrc.json"
+  },
+  settings     = {},
+}
