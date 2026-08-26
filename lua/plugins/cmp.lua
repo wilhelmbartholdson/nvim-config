86210bf28baa759f1b2bf83a9c7deda1b6bae3be
snillepille <155476536+snillepille@users.noreply.github.com>
2026-08-26 11:38:13 +0200

    use luasnip and migrate old nvim-cmp -> cmp.lua

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,34 +1,62 @@
 return {
   {
     "hrsh7th/nvim-cmp",
+    version = false,
     dependencies = {
       "neovim/nvim-lspconfig",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-buffer",
       "hrsh7th/cmp-path",
       "hrsh7th/cmp-cmdline",
-      "hrsh7th/cmp-vsnip",
-      "hrsh7th/vim-vsnip",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
+      "SergioRibera/cmp-dotenv",
+      "folke/lazydev.nvim",
+      "roobert/tailwindcss-colorizer-cmp.nvim",
     },
     config = function()
       local cmp = require("cmp")
 
+      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+
+      require("luasnip.loaders.from_vscode").lazy_load({
+        paths = vim.fn.stdpath("config") .. "/all_snippets",
+      })
+
       cmp.setup({
+        completion = {
+          completeopt = "menu,menuone,preview,noselect",
+        },
         snippet = {
           expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
+            require("luasnip").lsp_expand(args.body)
           end,
         },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        formatting = {
+          format = require("tailwindcss-colorizer-cmp").formatter,
+        },
         mapping = cmp.mapping.preset.insert({
+          ["<C-k>"] = cmp.mapping.select_prev_item(),
+          ["<C-j>"] = cmp.mapping.select_next_item(),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
+          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
+        experimental = {
+          ghost_text = true,
+        },
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "vsnip" },
+          { name = "lazydev" },
+          { name = "luasnip" },
+          { name = "dotenv" },
         }, {
           { name = "buffer" },
         }),
@@ -50,11 +78,6 @@ return {
         }),
         matching = { disallow_symbol_nonprefix_matching = false },
       })
-
-      -- Replace this placeholder with every LSP server you enable.
-      -- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      -- vim.lsp.config("<YOUR_LSP_SERVER>", { capabilities = capabilities })
-      -- vim.lsp.enable("<YOUR_LSP_SERVER>")
     end,
   },
 }
