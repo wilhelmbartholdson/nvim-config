02b606483739b3842a9e058859507282489b3fc1
snillepille <155476536+snillepille@users.noreply.github.com>
2026-08-26 11:40:31 +0200

    add floating commandline

---
+++ lua/plugins/cmdline.lua
@@ -0,0 +1,46 @@
+return {
+  "rachartier/tiny-cmdline.nvim",
+  dependencies = {},
+  enabled = true,
+  init = function()
+    vim.o.cmdheight = 0
+  end,
+  opts = {
+    -- Cmdline window width
+    width = {
+      value = "60%", -- "N%" = fraction of editor columns, integer = absolute columns
+      min = 40,      -- minimum width in columns
+      max = 80,      -- maximum width in columns
+    },
+
+    -- Window position ("N%" = fraction of available space, integer = absolute columns/rows)
+    position = {
+      x = "50%", -- horizontal: "0%" = left, "50%" = center, "100%" = right
+      y = "50%", -- vertical:   "0%" = top,  "50%" = center, "100%" = bottom
+    },
+
+    -- Border style for the floating window
+    -- nil inherits vim.o.winborder at setup() time, falling back to "rounded"
+    -- Set to "none" to disable the border
+    border = nil,
+
+    -- Horizontal offset of the completion menu anchor from the window's left inner edge
+    -- Used to align blink.cmp / nvim-cmp menus with the cmdline window
+    menu_col_offset = 3,
+
+    -- Cmdline types rendered at the bottom of the screen instead of centered
+    -- "/" and "?" (search) are kept native by default
+    native_types = { "/", "?" },
+
+    -- Dynamic popup title (rendered on the floating border)
+    -- Disabled by default; set enabled = true to opt in
+    -- Has no effect when border = "none" or when the cmdline is rendered via native_types
+    title = {
+      enabled = false,
+      pos = "center", -- "left" | "center" | "right"
+    },
+
+    -- Optional callback invoked after every reposition
+    on_reposition = nil,
+  }
+}
