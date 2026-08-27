3f0a59446b0a47b9ee82d3cbd0a00b8fd5a1d8b3
snillepille <155476536+snillepille@users.noreply.github.com>
2026-08-27 16:23:25 +0200

    formatting

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -19,9 +19,9 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     { import = "plugins" },
-    { import = "lsp" },
+    -- { import = "config.lsp" }
   },
 })
 
 -- initialize colorscheme
-vim.cmd[[colorscheme tokyonight-night]]
+vim.cmd [[colorscheme tokyonight-night]]
