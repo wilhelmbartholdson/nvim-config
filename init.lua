116bc6d2c7d0134b604543634f9217a1da07fa86
snillepille <155476536+snillepille@users.noreply.github.com>
2026-08-27 16:24:26 +0200

    enable ui2

--- init.lua
+++ init.lua
@@ -4,6 +4,7 @@
 require("config.keymaps")
 require("config.options")
 
+
 -- initialize lazy.nvim
 require("config.lazy")
 
@@ -21,6 +22,27 @@ vim.o.conceallevel = 1
 
 -- initialize lsp
 require("config.lsp")
+
+-- enable ui2
+require('vim._core.ui2').enable({
+  enable = true, -- Whether to enable or disable the UI.
+  msg = {        -- Options related to the message module.
+    ---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
+    ---or table mapping |ui-messages| kinds, triggers and IDs to a target.
+    ---Table keys are matched as a Lua pattern to the message ID. 'default'
+    ---mapping applies to any omitted kind: { default = 'cmd', progress = 'msg' }.
+    targets = 'cmd',
+    dialog = {        -- Options related to dialog window.
+      height = 0.5,   -- Maximum height.
+    },
+    msg = {           -- Options related to msg window.
+      height = 0.5,   -- Maximum height.
+    },
+    pager = {         -- Options related to message window.
+      height = 0.999, -- Maximum height.
+    },
+  },
+})
 
 -- nvim-treesitter
 vim.api.nvim_create_autocmd('FileType', {
