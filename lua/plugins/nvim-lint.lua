dd6ac1c8d4ebf72aec77b3c1ee0ec29df4901dc5
wiba <155476536+wilhelmbartholdson@users.noreply.github.com>
2026-09-08 18:42:16 +0200

    add r linter

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -5,6 +5,29 @@ return {
 
     local eslint = vim.fn.executable("eslint_d") == 1 and "eslint_d" or "eslint"
 
+    lint.linters.lintr = {
+      cmd = "/opt/homebrew/bin/R",
+      args = {
+        "--vanilla",
+        "--no-echo",
+        "-e",
+        "print(lintr::lint(commandArgs(trailingOnly = TRUE)))",
+        "--args",
+      },
+      stdin = false,
+      ignore_exitcode = true,
+      parser = require("lint.parser").from_pattern(
+        "(.-):(%d+):(%d+): (%a+): %[(.-)%] (.+)",
+        { "file", "lnum", "col", "severity", "code", "message" },
+        {
+          error = vim.diagnostic.severity.ERROR,
+          style = vim.diagnostic.severity.WARN,
+          warning = vim.diagnostic.severity.WARN,
+        },
+        { source = "lintr" }
+      ),
+    }
+
     lint.linters_by_ft = {
       javascript = { eslint },
       javascriptreact = { eslint },
@@ -12,13 +35,12 @@ return {
       typescriptreact = { eslint },
       vue = { eslint },
       svelte = { eslint },
+      r = { "lintr" },
     }
 
     vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost", "InsertLeave" }, {
       callback = function()
-        if vim.fn.executable(eslint) == 1 then
-          lint.try_lint()
-        end
+        lint.try_lint()
       end,
     })
   end,
