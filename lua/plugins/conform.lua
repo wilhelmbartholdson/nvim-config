2b1f088d454f9b26d54f9e88d3152618410e6543
wiba <155476536+wilhelmbartholdson@users.noreply.github.com>
2026-09-08 18:42:08 +0200

    add r formatter (styler)

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -17,9 +17,17 @@ return {
       yaml = { "prettier" },
       markdown = { "prettier" },
       markdown_inline = { "prettier" },
+      r = { "styler" },
+    },
+    formatters = {
+      styler = {
+        -- Prevent a project .Rprofile (for example, renv) from hiding the
+        -- globally installed styler package when Conform invokes R.
+        prepend_args = { "--no-init-file" },
+      },
     },
     format_on_save = function()
-      return { timeout_ms = 500, lsp_fallback = true }
+      return { timeout_ms = 1000, lsp_fallback = true }
     end,
   },
 }
