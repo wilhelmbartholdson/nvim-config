d78c757ebc5840dec2103b8a21a50d1c9a37a023
wiba <155476536+wilhelmbartholdson@users.noreply.github.com>
2026-09-08 18:30:30 +0200

    add inline diagnostics

---
+++ lua/plugins/diagnostic.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    opts = {
+      preset = "powerline",
+      options = {
+        multilines = { enabled = true }
+      }
+    },
+  }
+}
