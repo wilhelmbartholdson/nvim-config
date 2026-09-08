21e410cb16cf92fd05ad45210e1dc06f49f7145c
wiba <155476536+wilhelmbartholdson@users.noreply.github.com>
2026-09-08 15:07:22 +0200

    rm old blocks

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,46 +4,6 @@ return {
   build = ':TSUpdate',
   branch = 'main',
 
-  -- opts = {
-  --   -- LazyVim config for treesitter
-  --   indent = { enable = true }, ---@type lazyvim.TSFeat
-  --   highlight = { enable = true }, ---@type lazyvim.TSFeat
-  --   folds = { enable = true }, ---@type lazyvim.TSFeat
-  --   ensure_installed = {
-  --     "bash",
-  --     "c",
-  --     "diff",
-  --     "html",
-  --     "javascript",
-  --     "jsdoc",
-  --     "json",
-  --     "jsonc",
-  --     "lua",
-  --     "luadoc",
-  --     "luap",
-  --     "markdown",
-  --     "markdown_inline",
-  --     "printf",
-  --     "python",
-  --     "query",
-  --     "regex",
-  --     "toml",
-  --     "tsx",
-  --     "typescript",
-  --     "vim",
-  --     "vimdoc",
-  --     "xml",
-  --     "yaml",
-  --   },
-  -- }
-
-  -- opts = {
-  -- ensure_installed = { "python", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-  -- sync_install = false,
-  -- highlight = { enable = true },
-  -- indent = { enable = true },
-  -- },
-
   config = function()
     require 'nvim-treesitter'.install {
       "python",
@@ -65,34 +25,5 @@ return {
       "r",
       "css"
     }
-
-
-
-    -- require"nvim-treesitter".setup({
-    --   ensure_installed = {
-    --     "python",
-    --     "c",
-    -- "lua",
-    -- "vim",
-    --     "vimdoc",
-    --     "query",
-    --     "elixir",
-    --     "heex",
-    --     "javascript",
-    --     "typescript",
-    --     "html",
-    --     "markdown",
-    --     "markdown_inline",
-    --     "latex",
-    --     "regex"
-    --   },
-    --   sync_install = false,
-    --   highlight = {
-    --     enable = true, disable = {}
-    --   },
-    --   indent = { enable = true },
-    --   }
-    -- )
   end
-
 }
