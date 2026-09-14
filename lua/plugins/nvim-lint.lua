1746c81573bc0e17bec2a9c4039a7a20ed7cd8e0
wiba <155476536+wilhelmbartholdson@users.noreply.github.com>
2026-09-14 20:25:44 +0200

    don't manually call R linter

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -5,28 +5,28 @@ return {
 
     local eslint = vim.fn.executable("eslint_d") == 1 and "eslint_d" or "eslint"
 
-    lint.linters.lintr = {
-      cmd = "/opt/homebrew/bin/R",
-      args = {
-        "--vanilla",
-        "--no-echo",
-        "-e",
-        "print(lintr::lint(commandArgs(trailingOnly = TRUE)))",
-        "--args",
-      },
-      stdin = false,
-      ignore_exitcode = true,
-      parser = require("lint.parser").from_pattern(
-        "(.-):(%d+):(%d+): (%a+): %[(.-)%] (.+)",
-        { "file", "lnum", "col", "severity", "code", "message" },
-        {
-          error = vim.diagnostic.severity.ERROR,
-          style = vim.diagnostic.severity.WARN,
-          warning = vim.diagnostic.severity.WARN,
-        },
-        { source = "lintr" }
-      ),
-    }
+    -- lint.linters.lintr = {
+    --   cmd = "/opt/homebrew/bin/R",
+    --   args = {
+    --     "--vanilla",
+    --     "--no-echo",
+    --     "-e",
+    --     "print(lintr::lint(commandArgs(trailingOnly = TRUE)))",
+    --     "--args",
+    --   },
+    --   stdin = false,
+    --   ignore_exitcode = true,
+    --   parser = require("lint.parser").from_pattern(
+    --     "(.-):(%d+):(%d+): (%a+): %[(.-)%] (.+)",
+    --     { "file", "lnum", "col", "severity", "code", "message" },
+    --     {
+    --       error = vim.diagnostic.severity.ERROR,
+    --       style = vim.diagnostic.severity.WARN,
+    --       warning = vim.diagnostic.severity.WARN,
+    --     },
+    --     { source = "lintr" }
+    --   ),
+    -- }
 
     lint.linters_by_ft = {
       javascript = { eslint },
@@ -35,7 +35,7 @@ return {
       typescriptreact = { eslint },
       vue = { eslint },
       svelte = { eslint },
-      r = { "lintr" },
+      -- r = { "lintr" },
     }
 
     vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost", "InsertLeave" }, {
