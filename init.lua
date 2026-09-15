4921f90b4f0815d60d94cb2e8a4c081c978d57d2
wiba <155476536+wilhelmbartholdson@users.noreply.github.com>
2026-09-15 21:50:29 +0200

    formatting

--- init.lua
+++ init.lua
@@ -24,51 +24,51 @@ require("config.lsp")
 
 -- enable ui2
 require('vim._core.ui2').enable({
-    enable = true, -- Whether to enable or disable the UI.
-    msg = { -- Options related to the message module.
-        ---@type string | table<string, 'cmd' | 'msg' | 'pager'> Default message target
-        --- or table mapping |ui-messages| kinds, triggers and IDs to a target.
-        --- Table keys are matched as a Lua pattern to the message ID. 'default'
-        --- mapping applies to any omitted kind: { default = 'cmd', progress = 'msg' }.
-        targets = 'cmd',
-        dialog = { -- Options related to dialog window.
-            height = 0.5 -- Maximum height.
-        },
-        msg = { -- Options related to msg window.
-            height = 0.5 -- Maximum height.
-        },
-        pager = { -- Options related to message window.
-            height = 0.999 -- Maximum height.
-        }
-    }
+	enable = true, -- Whether to enable or disable the UI.
+	msg = { -- Options related to the message module.
+		---@type string | table<string, 'cmd' | 'msg' | 'pager'> Default message target
+		--- or table mapping |ui-messages| kinds, triggers and IDs to a target.
+		--- Table keys are matched as a Lua pattern to the message ID. 'default'
+		--- mapping applies to any omitted kind: { default = 'cmd', progress = 'msg' }.
+		targets = 'cmd',
+		dialog = { -- Options related to dialog window.
+			height = 0.5 -- Maximum height.
+		},
+		msg = { -- Options related to msg window.
+			height = 0.5 -- Maximum height.
+		},
+		pager = { -- Options related to message window.
+			height = 0.999 -- Maximum height.
+		}
+	}
 })
 
 -- set filetypes explicitly
 vim.filetype.add({
-    pattern = {
-        -- implicit ^ and $ is added so don't add manually
-        [".*tmux%.conf"] = "tmux"
-    }
+	pattern = {
+		-- implicit ^ and $ is added so don't add manually
+		[".*tmux%.conf"] = "tmux"
+	}
 })
 -- nvim-treesitter
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = {
-        'python',
-        'javascript',
-        'jsx',
-        'typescript',
-        'tsx',
-        'lua',
-        'html',
-        'css',
-        'markdown',
-        "r"
-    },
-    callback = function ()
-        vim.treesitter.start()
-        vim.wo[0][0].foldmethod = 'expr'
-        vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        vim.opt.foldenable = false -- disable folding on startup
-    end
+	pattern = {
+		'python',
+		'javascript',
+		'jsx',
+		'typescript',
+		'tsx',
+		'lua',
+		'html',
+		'css',
+		'markdown',
+		"r"
+	},
+	callback = function ()
+		vim.treesitter.start()
+		vim.wo[0][0].foldmethod = 'expr'
+		vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		vim.opt.foldenable = false -- disable folding on startup
+	end
 })
